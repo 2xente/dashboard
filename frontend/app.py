e764bba4387ffce95c7ce925da1422fbc410a5bf
2xente <lasserrebixente1@gmail.com>
2025-10-13 16:13:46 +0200

    retour au source

--- frontend/app.py
+++ frontend/app.py
@@ -6,6 +6,16 @@ import plotly.graph_objects as go
 import math
 from datetime import datetime
 import os
+import boto3
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_DEFAULT_REGION")
+)
+bucket = os.getenv("S3_BUCKET")
+
 
 # URL de l’API : prend la variable d’env (Azure), sinon fallback local
 API_URL = os.getenv("API_URL", "http://localhost:8000").rstrip("/")
